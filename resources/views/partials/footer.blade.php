768531124f01bd2c0d8082d40e01986b2856abaf
ismailbaoud <ibaoud46@gmail.com>
2025-03-14 10:52:00 +0000

    ajouter la section des troi persons qui'ent trouvent plus des velenurabilitys

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class=" text-black py-16">
+<footer class=" text-black py-16 mt-30">
     <div class="container mx-auto px-4">
         <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
             <!-- Brand Section -->
