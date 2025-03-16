18035dd822b83eb1c715fe181261511a385f7991
ismailbaoud <ibaoud46@gmail.com>
2025-03-16 02:37:05 +0000

    ajouter les route des pages register pour les entreprise et les hunter

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
 Route::get('/', [HomeController::class , 'index']);
+Route::get('/register_hunter', [AuthController::class , 'showRegisterHunter']);
+Route::get('/register_entreprise', [AuthController::class , 'showRegisterentreprise']);
