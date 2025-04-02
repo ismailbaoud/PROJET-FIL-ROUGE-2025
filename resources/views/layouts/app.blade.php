0d1256a1d9ad90cacbd9326add7aa118c647c499
ismailbaoud <ibaoud46@gmail.com>
2025-04-02 21:37:10 +0000

    modifier la page de base et ajouter tailwind coloure utiliser

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     @vite('resources/css/app.css')
+    <script>
+        tailwind.config = {
+          theme: {
+            extend: {
+              colors: {
+                success: '#22c55e',
+                info: '#0ea5e9',
+                warning: '#f59e0b',
+                danger: '#ef4444'
+                lightGray: "#f8f9fa",
+                mediumGray: "#e9ecef",
+                darkGray: "#666666",
+                darkBlue: "#0d1117",
+                green: "#28a745",
+                red: "#dc3545",
+                yellow: "#ffc107",
+                offWhite: "#f0f0f0"
+              }
+            }
+          }
+        }
+      </script>
 
 </head>
 <body>
-    @if(1 == 0)
+    @if(1 == 1)
 
     @include('partials.header')
     @include('partials.sidebar')
@@ -22,7 +45,7 @@
         @yield('main')
     
     </main>
-    @if(1 == 0)
+    @if(1 == 1)
 
     @include('partials.footer')
     @else
