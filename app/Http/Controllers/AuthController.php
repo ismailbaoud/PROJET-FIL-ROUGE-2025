91674e4447e7274d91d3bac86296544b3a93d3e6
ismailbaoud <ibaoud46@gmail.com>
2025-04-06 18:35:36 +0100

    Modifiez la méthode EnterpriseRegister et séparez la logique dans le service.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller {
 
     public function HunterRegister(HunterRegisterRequest $request ) {
 
-        $this->userService->createUserHunter($request);
+        $this->userService->createUserHunter($request->validated());
         return $this->showLogin();
     }
 
