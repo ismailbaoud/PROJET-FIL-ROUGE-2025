28b70d0b0dee187183cdcdf44d079dc90de0d729
ismailbaoud <ibaoud46@gmail.com>
2025-04-08 19:58:45 +0100

    ajouter le route de logout dans le sidebar d'entreprise

--- resources/views/partials/entreprise/sidebar.blade.php
+++ resources/views/partials/entreprise/sidebar.blade.php
@@ -1,6 +1,6 @@
-<aside class="w-64 text-black h-screen fixed flex flex-col">
+<aside class="w-64 text-black h-screen fixed flex flex-col ">
     <!-- Logo / Title -->
-    <div class="p-6 text-center border-b border-gray-700">
+    <div class="p-6 text-center">
         <h1 class="text-2xl font-bold tracking-wide">HappyHunt</h1>
     </div>
 
@@ -35,6 +35,13 @@
             <a href="/tr/settings" class="flex items-center gap-4 px-5 py-4 text-sm font-medium text-gray-700 bg-gray-50 rounded-xl shadow-md hover:bg-gray-100 hover:shadow-lg transition-all duration-300 transform hover:scale-[1.02]">
                 <i class="fas fa-cogs text-gray-700"></i> <!-- Settings Icon -->
                 <span>Settings</span>
+            </a>
+
+            <a>
+                <form method="POST" action="{{ route('logout') }}" class="flex items-center p- text-white rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>  
             </a>
         </div>
     </nav>
