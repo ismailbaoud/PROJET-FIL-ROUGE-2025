01523c0258af092cc74855c920c2726093e01e40
ismailbaoud <ibaoud46@gmail.com>
2025-04-08 20:01:34 +0100

    former le code do page header de page home

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,27 +1,29 @@
+<header
+    class="max-w-screen  px-4 py-4 flex items-center justify-between  bg-gradient-to-none from-white via-white via-30% to-green-700 fixed z-9999 left-0 right-0 bg-white shadow-sm">
+    <div class="font-bold text-2xl">
+        <a href="">HappyHunt</a>
+    </div>
 
-    <header class="max-w-screen  px-4 py-4 flex items-center justify-between  bg-gradient-to-none from-white via-white via-30% to-green-700 fixed z-9999 left-0 right-0 bg-white shadow-sm">
-        <div class="font-bold text-2xl">
-            <a href="">HappyHunt</a>
-        </div>
-        
-        <nav class="hidden md:flex items-center space-between space-x-80">
-            <a href="/" class="text-base font-medium hover:underline">
-                Leaderboard
-            </a>
-            <a href="" class="text-base font-medium hover:underline">
-                Reports
-            </a>
-            <a href="" class="text-base font-medium hover:underline">
-                About
-            </a>
-        </nav>
-        
-        <div class="flex items-center space-x-4">
-            <a href="/loginPage" class="inline-flex items-center justify-center rounded-md border border-black bg-white px-4 py-2 text-sm font-medium text-black hover:bg-gray-100">
-                Login
-            </a>
-            <a href="/register_hunter" class="inline-flex items-center justify-center rounded-md bg-black px-4 py-2 text-sm font-medium text-white hover:bg-black/90">
-                Join the Hunt
-            </a>
-        </div>
-    </header>
+    <nav class="hidden md:flex items-center space-between space-x-80">
+        <a href="/" class="text-base font-medium hover:underline">
+            Leaderboard
+        </a>
+        <a href="" class="text-base font-medium hover:underline">
+            Reports
+        </a>
+        <a href="" class="text-base font-medium hover:underline">
+            About
+        </a>
+    </nav>
+
+    <div class="flex items-center space-x-4">
+        <a href="/loginPage"
+            class="inline-flex items-center justify-center rounded-md border border-black bg-white px-4 py-2 text-sm font-medium text-black hover:bg-gray-100">
+            Login
+        </a>
+        <a href="/register_hunter"
+            class="inline-flex items-center justify-center rounded-md bg-black px-4 py-2 text-sm font-medium text-white hover:bg-black/90">
+            Join the Hunt
+        </a>
+    </div>
+</header>
