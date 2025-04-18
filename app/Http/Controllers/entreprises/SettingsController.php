d2aeae0f63bce3f6b7019d87264ee49395a011aa
ismailbaoud <ibaoud46@gmail.com>
2025-04-18 23:57:27 +0100

    feat: create SettingsController.php

---
+++ app/Http/Controllers/entreprises/SettingsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\entreprises;
+
+use Illuminate\Http\Request;
+use App\Models\Scope;
+use App\Models\Program;
+use App\Http\Controllers\Controller;
+
+class SettingsController extends Controller
+{
+   public function index(){
+    return view('pages.entreprise/settings');
+   }
+}
