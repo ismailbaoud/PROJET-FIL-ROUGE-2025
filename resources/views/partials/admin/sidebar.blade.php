54a6c175fb8638b4a8008c7d58a124fe37048a46
ismailbaoud <ibaoud46@gmail.com>
2025-04-23 16:12:38 +0100

    ajouter des modification dans les partials d'admin

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -4,58 +4,60 @@
     </div>
     <ul class="space-y-4 px-4">
         <li>
-            <a href="/dm/dashboard" class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
+            <a href="/dm/dashboard"
+                class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
                 <i class="fas fa-tachometer-alt text-xl"></i>
                 <span class="ml-3">Dashboard</span>
             </a>
         </li>
         <li>
-            <a href="/dm/users" class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
+            <a href="/dm/users"
+                class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
                 <i class="fas fa-users text-xl"></i>
                 <span class="ml-3">Utilisateurs</span>
             </a>
         </li>
         <li>
-            <a href="/dm/programs" class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
+            <a href="/dm/programs"
+                class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
                 <i class="fas fa-cogs text-xl"></i>
                 <span class="ml-3">Programmes</span>
             </a>
         </li>
         <li>
-            <a href="/dm/reports" class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
+            <a href="/dm/reports"
+                class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
                 <i class="fas fa-clipboard-list text-xl"></i>
                 <span class="ml-3">Rapports</span>
             </a>
         </li>
         <li>
-            <a href="/dm/transactions" class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
+            <a href="/dm/transactions"
+                class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
                 <i class="fas fa-exchange-alt text-xl"></i>
                 <span class="ml-3">Transactions</span>
             </a>
         </li>
         <li>
-            <a href="/dm/green-rooms" class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
-                <i class="fas fa-tree text-xl"></i>
-                <span class="ml-3">Green Rooms</span>
-            </a>
-        </li>
-        <li>
-            <a href="/dm/logs" class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
+            <a href="/dm/logs"
+                class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
                 <i class="fas fa-file-alt text-xl"></i>
                 <span class="ml-3">Logs</span>
             </a>
         </li>
         <li>
-            <a href="/dm/settings" class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
+            <a href="/dm/settings"
+                class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
                 <i class="fas fa-cogs text-xl"></i>
                 <span class="ml-3">Paramètres</span>
             </a>
         </li>
         <li>
-                <form method="POST" action="{{ route('logout') }}" class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
-                    @csrf
-                    <button type="submit">Logout</button>
-                </form>  
+            <form method="POST" action="{{ route('logout') }}"
+                class="flex items-center p-4 rounded-lg bg-gray-800 shadow-md hover:shadow-lg hover:bg-gray-700 transition duration-300">
+                @csrf
+                <button type="submit">Logout</button>
+            </form>
         </li>
     </ul>
 </nav>
