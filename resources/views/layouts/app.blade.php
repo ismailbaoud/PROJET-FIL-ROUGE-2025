fe3effeb924bcf26e25fb2b6cf77729d23714478
ismailbaoud <ibaoud46@gmail.com>
2025-04-23 16:06:51 +0100

    ajouter des modification dans les layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,46 +8,45 @@
     @vite('resources/css/app.css')
     <script>
         tailwind.config = {
-          theme: {
-            extend: {
-              colors: {
-                success: '#22c55e',
-                info: '#0ea5e9',
-                warning: '#f59e0b',
-                danger: '#ef4444'
-                lightGray: "#f8f9fa",
-                mediumGray: "#e9ecef",
-                darkGray: "#666666",
-                darkBlue: "#0d1117",
-                green: "#28a745",
-                red: "#dc3545",
-                yellow: "#ffc107",
-                offWhite: "#f0f0f0"
-              }
+            theme: {
+                extend: {
+                    colors: {
+                        success: '#22c55e',
+                        info: '#0ea5e9',
+                        warning: '#f59e0b',
+                        danger: '#ef4444'
+                        lightGray: "#f8f9fa",
+                        mediumGray: "#e9ecef",
+                        darkGray: "#666666",
+                        darkBlue: "#0d1117",
+                        green: "#28a745",
+                        red: "#dc3545",
+                        yellow: "#ffc107",
+                        offWhite: "#f0f0f0"
+                    }
+                }
             }
-          }
         }
-      </script>
+    </script>
 
 </head>
+
 <body>
 
-@guest
-@include('partials.header')
-@include('partials.sidebar')
-@endguest
-
-{{-- @dd(Auth::user()) --}}
+    @guest
+        @include('partials.header')
+    @endguest
 
     <main>
-          
-          
+
         @yield('main')
-    
+
     </main>
 
     @include('partials.footer')
 
     @vite('resources/js/app.js')
+
 </body>
+
 </html>
