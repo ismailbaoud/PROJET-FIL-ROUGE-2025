9836e5583f1077ae018f04c8b5108f2a772b2b71
ismailbaoud <ibaoud46@gmail.com>
2025-04-24 23:11:06 +0100

    ajouter App/main pour handle l'envoi d'email  et randrer la page que contien le context d'email

---
+++ app/Mail/ReportStatusChanged.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Mail;
+
+use App\Models\Report;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ReportStatusChanged extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $report;
+
+    public function __construct(Report $report)
+    {
+        $this->report = $report;
+    }
+
+    public function build()
+    {
+        return $this->subject('Status of Your Report Has Changed')
+                    ->view('emails.report_status_changed');
+    }
+}
