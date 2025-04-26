a6698aec54d24b819088a6b3f5599e141357e83b
ismailbaoud <ibaoud46@gmail.com>
2025-04-26 22:34:49 +0100

    ajouter la fonction index de controleur leaderboard pour afficher le rank des utilisateurs

--- app/Http/Controllers/hunters/LeaderboardController.php
+++ app/Http/Controllers/hunters/LeaderboardController.php
@@ -4,15 +4,34 @@ namespace App\Http\Controllers\hunters;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Profile;
 
 class LeaderboardController extends Controller
 {
 
     //index
-    public function index(){
 
-        return view('pages.hunter/leaderboard');
+    public function index(Request $request)
+{
+    $sortField = $request->input('filter', 'pointes'); 
+
+    if (!in_array($sortField, ['pointes', 'rewards'])) {
+        $sortField = 'pointes'; 
     }
+
+    $profiles = Profile::with('user')
+    ->whereHas('user', function($query) {
+        $query->where('role', 'hunter');
+    })
+    ->orderByDesc($sortField)
+    ->get();
+
+    foreach ($profiles as $index => $profile) {
+        $profile->rank = $index + 1;
+
+    }
+    return view('pages.hunter/leaderboard', compact('profiles', 'sortField'));
+}
 
 
 }
