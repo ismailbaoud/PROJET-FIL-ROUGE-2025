7b6c67970c58da8b48e4993535e4afc3ff00c66d
ismailbaoud <ibaoud46@gmail.com>
2025-04-26 22:28:43 +0100

    ajouter le controleur de report de partie admin

---
+++ app/Http/Controllers/admin/ReportController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Models\Report;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+class ReportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $reports = Report::with('program')->get();
+        return view('pages.admin/reports', compact('reports'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Report $report)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Report $report)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Report $report)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Report $report)
+    {
+        $report->delete();
+        return back();
+    }
+}
