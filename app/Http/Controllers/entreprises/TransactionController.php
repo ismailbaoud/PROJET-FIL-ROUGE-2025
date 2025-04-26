43feeb8ae80ee0dedadcc1bf6a1af9b9d21d44ee
ismailbaoud <ibaoud46@gmail.com>
2025-04-26 22:32:37 +0100

    ajouter la page des transactions de partier entreprise

---
+++ app/Http/Controllers/entreprises/TransactionController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\entreprises;
+
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $transactions = Transaction::with(['toUser', 'fromUser', 'report', 'program'])->where('from_user_id', Auth::Id())->get();
+        return view('pages.entreprise/transactions', compact('transactions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Transaction $transaction)
+    {
+        //
+    }
+}
