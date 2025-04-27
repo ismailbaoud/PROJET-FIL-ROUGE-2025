0a4d94014822221efe56b17c750c7ca837a8dcb4
ismailbaoud <ibaoud46@gmail.com>
2025-04-27 23:35:41 +0100

    ajouter l'affichage des error aprer la validation des input dans le form de registration des entreprises

--- resources/views/auth/register_entreprise.blade.php
+++ resources/views/auth/register_entreprise.blade.php
@@ -31,25 +31,37 @@
                         <label for="companyName" class="block text-gray-700">Company Name</label>
                         <input id="companyName" name="companyName" type="text"
                             class="w-full h-12 px-3 bg-white rounded-md border border-black focus:outline-none focus:ring-1 focus:ring-gray-500" />
+                        @error('companyName')
+                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        @enderror
                     </div>
 
                     <div class="space-y-2">
                         <label for="accountUrl" class="block text-gray-700">Account URL</label>
                         <input id="accountUrl" name="companyUrl" type="text"
                             class="w-full h-12 px-3 bg-white rounded-md border border-black focus:outline-none focus:ring-1 focus:ring-gray-500" />
-                    </div>
+                            @error('companyUrl')
+                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                        </div>
 
                     <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
                         <div class="space-y-2">
                             <label for="country" class="block text-gray-700">Country</label>
                             <input id="country" name="country" type="text"
                                 class="w-full h-12 bg-white px-3 rounded-md border border-black focus:outline-none focus:ring-1 focus:ring-gray-500" />
-                        </div>
+                                @error('country')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                            @enderror
+                            </div>
                         <div class="space-y-2">
                             <label for="state" class="block text-gray-700">State</label>
                             <input id="state" name="state" type="text"
                                 class="w-full h-12 bg-white px-3 rounded-md border border-black focus:outline-none focus:ring-1 focus:ring-gray-500" />
-                        </div>
+                                @error('state')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                            @enderror
+                            </div>
                     </div>
                 </div>
 
@@ -60,30 +72,32 @@
                         <label for="fullName" class="block text-gray-700">Full Name</label>
                         <input id="fullName" name="fullName" type="text"
                             class="w-full h-12 px-3 bg-white rounded-md border border-black focus:outline-none focus:ring-1 focus:ring-gray-500" />
-                    </div>
+                            @error('fullName')
+                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                        </div>
 
                     <div class="space-y-2">
                         <label for="businessEmail" class="block text-gray-700">Business Email</label>
                         <input id="businessEmail" name="businessEmail" type="email"
                             class="w-full h-12 bg-white px-3 rounded-md border border-black focus:outline-none focus:ring-1 focus:ring-gray-500" />
-                    </div>
+                            @error('businessEmail')
+                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                        </div>
 
                     <div class="space-y-2">
                         <label for="password" class="block text-gray-700">Password</label>
                         <input id="password" name="password" type="password"
                             class="w-full h-12 bg-white px-3 rounded-md border border-black focus:outline-none focus:ring-1 focus:ring-gray-500" />
-                    </div>
+                            @error('password')
+                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                        </div>
                 </div>
 
                 <div class="md:col-span-2 space-y-6 mt-4 text-center">
-                    <div class="flex items-center space-x-2 justify-center">
-                        <input type="checkbox" id="terms"
-                            class="h-4 w-4 rounded border-gray-300 text-black focus:ring-0" />
-                        <label for="terms" class="text-sm font-medium text-gray-700">
-                            I agree to the Terms & Conditions
-                        </label>
-                    </div>
-
+                   
                     <div class="flex justify-center">
                         <button type="submit"
                             class="w-full md:w-auto px-8 py-4 bg-black text-white rounded-md hover:bg-gray-800 transition-colors">
