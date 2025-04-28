353cfb1ff05afff968c0a8efbaed7e3dda0988ab
ismailbaoud <ibaoud46@gmail.com>
2025-04-28 23:06:05 +0100

    supremer les fonction non utiliser

--- app/Http/Controllers/admin/SettingsController.php
+++ app/Http/Controllers/admin/SettingsController.php
@@ -18,38 +18,6 @@ class SettingsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Profile $profile)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Profile $profile)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
     public function update(Request $request,User $user)
